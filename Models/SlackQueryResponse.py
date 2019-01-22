bd29d1b7f9680860b1e4543c8b17ecb36549851b
AJ Tatum <andrew.j.tatum@gmail.com>
2019-01-22 15:38:39 -0500

    Cleaned up app to consolidate code. Handle unfound errors better

--- Models/SlackQueryResponse.py
+++ Models/SlackQueryResponse.py
@@ -15,11 +15,21 @@ class SlackQueryResponse(object):
         pass
 
     def FromJson(self, json):
-        self.fallback = json['items'][0]['snippet'].replace('\n','')
-        self.text = json['items'][0]['snippet'].replace('\n',''),
-        self.title = json['items'][0]['title'],
-        self.title_link = json['items'][0]['link'],
-        self.image_url = json['items'][0]['pagemap']['metatags'][0]['og:image'],
+        total_items = json['queries']['request'][0]['totalResults']
+        search_term = json['queries']['request'][0]['searchTerms']
+
+        if total_items != "0":
+            self.fallback = json['items'][0]['snippet'].replace('\n','')
+            self.text = json['items'][0]['snippet'].replace('\n',''),
+            self.title = json['items'][0]['title'],
+            self.title_link = json['items'][0]['link'],
+            self.image_url = json['items'][0]['pagemap']['metatags'][0]['og:image']
+        else:
+            self.fallback = "Unable to find anything about: {}".format(search_term)
+            self.text = "Ut oh!"
+            self.title = self.fallback
+            self.title_link = "https://www.marvel.com/"
+            self.image_url = self.footer_icon
 
     def ToText(self):
         return "*{}*\n{}\n{}\n{}".format(
@@ -31,14 +41,14 @@ class SlackQueryResponse(object):
     def ToJson(self):
         attachments = [
                 {
-                    "fallback": " ".join(str(x) for x in self.text),
+                    "fallback": "".join(str(x) for x in self.text),
                     "color": self.color,
                     "author_name": self.author_name,
                     "author_link": self.author_link,
-                    "text": " ".join(str(x) for x in self.text),
-                    "title": " ".join(str(x) for x in self.title),
-                    "title_link": " ".join(str(x) for x in self.title_link),
-                    "image_url": " ".join(str(x) for x in self.image_url),
+                    "text": "".join(str(x) for x in self.text),
+                    "title": "".join(str(x) for x in self.title),
+                    "title_link": "".join(str(x) for x in self.title_link),
+                    "image_url": "".join(str(x) for x in self.image_url),
                     "footer": self.footer,
                     "footer_icon": self.footer_icon,
                 }
